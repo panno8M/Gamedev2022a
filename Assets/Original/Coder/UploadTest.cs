6dd0dc71ce6a0226b1ddae017c1dc5338997e68b
kabutogani <a20dc119@dhw.ac.jp>
2022-10-31 21:36:48 +0900

    ちゃんとプッシュできるかのテスト

---
+++ Assets/Original/Coder/UploadTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UploadTest : MonoBehaviour
+{
+    //てすとテストtestTest
+    void Start(){
+        Debug.Log("TEEEEEESTTTTT");
+    }
+}
