b1598515a809a67cf20aef88b6f17f27c2a86a84
panno <panno.800k@gmail.com>
2022-10-31 18:42:22 +0900

    InputSystemに完全移行

--- Assets/Original/Coder/Scripts/Components/UI/DebugUI.cs
+++ Assets/Original/Coder/Scripts/Components/UI/DebugUI.cs
@@ -1,48 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.UI;
 using UniRx;
+using UniRx.Triggers;
 
 public class DebugUI : MonoBehaviour
 {
 
-    public GameObject RightUI;
-    public GameObject LeftUI;
-    public GameObject JumpUI;
+    public Image RightUI;
+    public Image LeftUI;
+    public Image JumpUI;
+
+    Color white = new Color(1f, 1f, 1f);
+    Color gray = new Color(.5f, .5f, .5f);
     [SerializeField] Text _uiTextPlayerDmg;
 
-    int dmgTotal;
-
     void Start() {
-        Player.Instance.Damagable.OnDamage.Subscribe(dmg => {
-            dmgTotal += dmg;
-            _uiTextPlayerDmg.text = dmgTotal.ToString();
+        Player.Instance.Damagable.OnDamage
+            .Scan((total, current) => total + current)
+            .Select(x => x.ToString())
+            .Subscribe(x => _uiTextPlayerDmg.text = x)
+            .AddTo(this);
+
+        Player.Control.HorizontalMoveInput.Subscribe(hmi => {
+            LeftUI.color = (hmi == -1) ? gray : white;
+            RightUI.color = (hmi == 1) ? gray : white;
         }).AddTo(this);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey("a")) {
-            LeftUI.GetComponent<Image>().color = new Color(0.5f,0.5f,0.5f);
-            
-        }else{
-            LeftUI.GetComponent<Image>().color = new Color(1f,1f,1f);
-        }
-
-        if(Input.GetKey("d")) {
-            RightUI.GetComponent<Image>().color = new Color(0.5f,0.5f,0.5f);
-            
-        }else{
-            RightUI.GetComponent<Image>().color = new Color(1f,1f,1f);
-        }
-
-        if(Input.GetKey("space")) {
-            JumpUI.GetComponent<Image>().color = new Color(0.5f,0.5f,0.5f);
-            
-        }else{
-            JumpUI.GetComponent<Image>().color = new Color(1f,1f,1f);
-        }
+        Player.Control.input.Player.GoUp.started +=
+            context => JumpUI.color = context.ReadValue<float>() == 1 ? gray : white;
+        Player.Control.input.Player.GoUp.canceled +=
+            context => JumpUI.color = context.ReadValue<float>() == 1 ? gray : white;
     }
 }
