7d678716e69dd92698d31f4aecd7c2cd461fbebc
panno <panno.800k@gmail.com>
2022-11-06 21:38:27 +0900

    レイヤー名を調整

--- Assets/Original/Coder/Scripts/Pure/LayerWrapper.cs
+++ Assets/Original/Coder/Scripts/Pure/LayerWrapper.cs
@@ -1,18 +1,21 @@
 using System.ComponentModel;
 using UnityEngine;
 public enum Layer {
-    [Description("Default")]                Default,
-    [Description("TransparentFX")]          TransparentFX,
-    [Description("Ignore Raycast")]         IgnoreRaycast,
-    [Description("Player")]                 Player,
-    [Description("Water")]                  Water,
-    [Description("UI")]                     UI,
-    [Description("Stage")]                  Stage,
-    [Description("Enemy")]                  Enemy,
-    [Description("AiControlVolume")]        AiControlVolume,
-    [Description("AiVisible")]              AiVisible,
-    [Description("Damagable")]              Damagable,
-    [Description("ScreenToStageConverter")] ScreenToStageConverter,
+    [Description("Default")]                    Default,
+    [Description("TransparentFX")]              TransparentFX,
+    [Description("Ignore Raycast")]             IgnoreRaycast,
+    [Description("Physics/Dynamics")]           Dynamics,
+    [Description("Water")]                      Water,
+    [Description("UI")]                         UI,
+    [Description("Physics/Stage")]              Stage,
+    [Description("Enemy")]                      Enemy,
+    [Description("AiControlVolume")]            AiControlVolume,
+    [Description("Senses/AiVisible")]           AiVisible,
+    [Description("Senses/Damagable")]           Damagable,
+    [Description("ScreenToStageConverter")]     ScreenToStageConverter,
+    [Description("Senses/DamagableFromPlayer")] DamagableFromPlayer,
+    [Description("Senses/Interactable")]        Interactable,
+    [Description("Senses/Interactor")]          Interactor,
     }
 
 public struct Layers {
