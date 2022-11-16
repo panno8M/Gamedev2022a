c2c2c72afdc8c780e61a4e9ece25a464b14314d1
panno <panno.800k@gmail.com>
2022-11-16 21:38:55 +0900

    オーバーヒートとクールダウンを暫定的に実装

--- Assets/Original/Coder/Scripts/Components/Actors/Player/Behaviour/PlayerBreath.cs
+++ Assets/Original/Coder/Scripts/Components/Actors/Player/Behaviour/PlayerBreath.cs
@@ -9,25 +9,40 @@ namespace Assembly.Components.Actors.Player
   {
     [SerializeField] PlayerAct _player;
     [SerializeField] ParticleSystem psFlameBreath;
-    [SerializeField] float msecExhalableLimit = 3000;
 
     ReactiveProperty<bool> _IsExhaling = new ReactiveProperty<bool>();
     public ReactiveProperty<bool> IsExhaling => _IsExhaling;
     public IObservable<bool> OnBreathStart => _IsExhaling.Where(x => x);
     public IObservable<bool> OnBreathStop => _IsExhaling.Where(x => !x);
 
+
+    [SerializeField] float msecExhalableLimit = 3000;
     [SerializeField] float msecExhaling;
-    float msecNeedsRest => msecExhaling;
+    [SerializeField] float msecCooldown;
 
     void Awake()
     {
       this.FixedUpdateAsObservable()
-          .Where(_ => IsExhaling.Value)
           .TimeInterval()
+          .Where(_ => IsExhaling.Value)
           .Subscribe(delta =>
           {
             psFlameBreath.transform.LookAt(Global.Control.MousePosStage.Value);
             msecExhaling += delta.Interval.Milliseconds;
+          }).AddTo(this);
+
+      this.FixedUpdateAsObservable()
+          .FrameTimeInterval()
+          .Where(_ => isCoolingDown)
+          .Subscribe(delta =>
+          {
+            msecCooldown -= delta.Interval.Milliseconds;
+
+            if (msecCooldown <= 0)
+            {
+              msecCooldown = 0;
+              RemoveOveruseLimitation();
+            }
           }).AddTo(this);
 
       IsExhaling
@@ -36,30 +51,56 @@ namespace Assembly.Components.Actors.Player
             if (b)
             {
               psFlameBreath.Play();
-              _player.flapCtl.TightenLimit(0);
+              SetOveruseLimitation();
             }
             else
             {
-              psFlameBreath.Stop();
-              _player.flapCtl.ResetLimit();
-              msecExhaling = 0;
-
+              CooldownStart();
             }
           }).AddTo(this);
 
       Global.Control.BreathPress
           .Where(_ => !_player.interactor.holder.HoldingItem.Value)
+          .Where(_ => !isCoolingDown)
           .Subscribe(_ => _IsExhaling.Value = true)
           .AddTo(this);
 
       Global.Control.BreathRelease
+          .Where(_ => !isCoolingDown)
           .Subscribe(_ => _IsExhaling.Value = false)
           .AddTo(this);
       _player.interactor.holder.RequestHold
           .Where(_ => Global.Control.BreathInput.Value)
+          .Where(_ => !isCoolingDown)
+          .Subscribe(_ => _IsExhaling.Value = false)
+          .AddTo(this);
+      this.FixedUpdateAsObservable()
+          .Where(_ => msecExhaling > msecExhalableLimit)
+          .Where(_ => !isCoolingDown)
           .Subscribe(_ => _IsExhaling.Value = false)
           .AddTo(this);
     }
+
+
+    void SetOveruseLimitation()
+    {
+      _player.flapCtl.TightenLimit(0);
+    }
+
+    void RemoveOveruseLimitation()
+    {
+      _player.flapCtl.ResetLimit();
+    }
+
+
+    bool isCoolingDown => msecCooldown != 0;
+    void CooldownStart()
+    {
+      psFlameBreath.Stop();
+      msecCooldown = msecExhaling;
+      msecExhaling = 0;
+    }
+
 
     void Reset()
     {
