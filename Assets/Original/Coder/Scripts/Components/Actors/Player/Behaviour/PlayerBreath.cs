c90b74d5f7f346bb29e1fe5d703b71220d4ca564
panno <panno.800k@gmail.com>
2022-11-16 20:36:29 +0900

    ブレス関連のイベントを見直し、拡張しやすく

--- Assets/Original/Coder/Scripts/Components/Actors/Player/Behaviour/PlayerBreath.cs
+++ Assets/Original/Coder/Scripts/Components/Actors/Player/Behaviour/PlayerBreath.cs
@@ -11,37 +11,54 @@ namespace Assembly.Components.Actors.Player
     [SerializeField] ParticleSystem psFlameBreath;
     [SerializeField] float msecExhalableLimit = 3000;
 
-    IObservable<Unit> _onBreathStart;
-    IObservable<Unit> _onBreathStop;
-    public IObservable<Unit> OnBreathStart => _onBreathStart ??
-        (_onBreathStart = Global.Control.BreathPress
-            .Where(_ => !_player.interactor.holder.HoldingItem.Value));
+    ReactiveProperty<bool> _IsExhaling = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> IsExhaling => _IsExhaling;
+    public IObservable<bool> OnBreathStart => _IsExhaling.Where(x => x);
+    public IObservable<bool> OnBreathStop => _IsExhaling.Where(x => !x);
 
-    public IObservable<Unit> OnBreathStop => _onBreathStop ??
-        (_onBreathStop = Observable.Merge(
-            Global.Control.BreathRelease,
-            _player.interactor.holder.RequestHold.AsUnitObservable()
-                .Where(_ => Global.Control.BreathInput.Value)));
-
+    [SerializeField] float msecExhaling;
+    float msecNeedsRest => msecExhaling;
 
     void Awake()
     {
       this.FixedUpdateAsObservable()
-          .Where(_ => Global.Control.BreathInput.Value)
-          .Subscribe(_ =>
+          .Where(_ => IsExhaling.Value)
+          .TimeInterval()
+          .Subscribe(delta =>
           {
             psFlameBreath.transform.LookAt(Global.Control.MousePosStage.Value);
+            msecExhaling += delta.Interval.Milliseconds;
           }).AddTo(this);
 
-      OnBreathStart
-          .Subscribe(_ => psFlameBreath.Play()).AddTo(this);
-      OnBreathStop
-          .Subscribe(_ => psFlameBreath.Stop()).AddTo(this);
+      IsExhaling
+          .Subscribe(b =>
+          {
+            if (b)
+            {
+              psFlameBreath.Play();
+              _player.flapCtl.OverrideLimit(0);
+            }
+            else
+            {
+              psFlameBreath.Stop();
+              _player.flapCtl.ResetLimit();
+              msecExhaling = 0;
 
-      OnBreathStart
-          .Subscribe(_ => _player.flapCtl.OverrideLimit(0)).AddTo(this);
-      OnBreathStop
-          .Subscribe(_ => _player.flapCtl.ResetLimit()).AddTo(this);
+            }
+          }).AddTo(this);
+
+      Global.Control.BreathPress
+          .Where(_ => !_player.interactor.holder.HoldingItem.Value)
+          .Subscribe(_ => _IsExhaling.Value = true)
+          .AddTo(this);
+
+      Global.Control.BreathRelease
+          .Subscribe(_ => _IsExhaling.Value = false)
+          .AddTo(this);
+      _player.interactor.holder.RequestHold
+          .Where(_ => Global.Control.BreathInput.Value)
+          .Subscribe(_ => _IsExhaling.Value = false)
+          .AddTo(this);
     }
 
     void Reset()
