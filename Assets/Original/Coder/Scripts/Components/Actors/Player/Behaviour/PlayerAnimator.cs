dcca814c032ac27faa79e7ff9a803542e25ce9f4
panno <panno.800k@gmail.com>
2022-11-18 15:36:35 +0900

    ものを持つアニメーションを適用

--- Assets/Original/Coder/Scripts/Components/Actors/Player/Behaviour/PlayerAnimator.cs
+++ Assets/Original/Coder/Scripts/Components/Actors/Player/Behaviour/PlayerAnimator.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UniRx;
+using UniRx.Ex.InteractionTraits;
 
 namespace Assembly.Components.Actors.Player
 {
   public class PlayerAnimator : MonoBehaviour
   {
+    [SerializeField] PlayerAct _player;
     [SerializeField] Animator _anim;
     void Awake()
     {
       Global.Control.HorizontalMoveInput
           .Subscribe(Walk)
+          .AddTo(this);
+
+      _player.interactor.holder.HoldingItem
+          .Subscribe(Hold)
           .AddTo(this);
     }
 
@@ -20,6 +26,11 @@ namespace Assembly.Components.Actors.Player
     void Walk(float hmi)
     {
       Walk(hmi != 0);
+    }
+
+    void Hold(HoldableModule holdable)
+    {
+      _anim.SetBool("Grab", holdable);
     }
   }
 }
