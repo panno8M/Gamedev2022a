90746bed9dc0397b58746d4a1bbf49f58a2acddd
panno <panno.800k@gmail.com>
2022-12-06 13:47:09 +0900

    感圧版の上に乗ったオブジェクトが消失した際に元に戻らない不具合を修正

--- Assets/Original/Coder/Scripts/Components/StageGimmicks/PressurePlate.cs
+++ Assets/Original/Coder/Scripts/Components/StageGimmicks/PressurePlate.cs
@@ -1,59 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 using Cysharp.Threading.Tasks;
 
-public class PressurePlate : MonoBehaviour
+namespace Assembly.Components.StageGimmicks
 {
-
-  List<Collider> riddenCounter = new List<Collider>();
-
-  enum Mode { Relax = -1, Press = 1 }
-  Mode targetMode = Mode.Relax;
-  [SerializeField]
-  EzLerp animateProgress = new EzLerp(1);
-
-  [SerializeField] GameObject _plateObject;
-  Material _plateMaterial;
-  Vector3 _positionDefault;
-  [SerializeField] Vector3 _positionDelta;
-  Color _relaxColor;
-  [SerializeField] Color _pressColor;
-
-
-  void Start()
+  [RequireComponent(typeof(SafetyTrigger))]
+  public class PressurePlate : MonoBehaviour
   {
-    _positionDefault = _plateObject.transform.localPosition;
-    _plateMaterial = _plateObject.GetComponent<Renderer>().material;
-    _relaxColor = _plateMaterial.color;
-    AnimatePress().Forget();
-  }
-  void OnTriggerEnter(Collider other)
-  {
-    riddenCounter.Add(other);
-    targetMode = Mode.Press;
-  }
-  void OnTriggerExit(Collider other)
-  {
-    riddenCounter.RemoveAll(x => x == other);
-    if (riddenCounter.Count == 0)
+    SafetyTrigger SafetyTrigger;
+
+    enum Mode { Relax = -1, Press = 1 }
+    Mode targetMode = Mode.Relax;
+    [SerializeField]
+    EzLerp animateProgress = new EzLerp(1);
+
+    [SerializeField] GameObject _plateObject;
+    Material _plateMaterial;
+    Vector3 _positionDefault;
+    [SerializeField] Vector3 _positionDelta;
+    Color _relaxColor;
+    [SerializeField] Color _pressColor;
+
+
+    void Start()
     {
-      targetMode = Mode.Relax;
+      SafetyTrigger = GetComponent<SafetyTrigger>();
+      _positionDefault = _plateObject.transform.localPosition;
+      _plateMaterial = _plateObject.GetComponent<Renderer>().material;
+      _relaxColor = _plateMaterial.color;
+      AnimatePress().Forget();
+
+      SafetyTrigger.Triggers.ObserveAdd()
+        .Subscribe(trigger =>
+        {
+          targetMode = Mode.Press;
+        }).AddTo(this);
+      SafetyTrigger.Triggers.ObserveRemove()
+        .Subscribe(trigger =>
+        {
+          if (SafetyTrigger.Triggers.Count == 0)
+          {
+            targetMode = Mode.Relax;
+          }
+        }).AddTo(this);
     }
-  }
 
-  async UniTask AnimatePress()
-  {
-    while (Application.isPlaying)
+    async UniTask AnimatePress()
     {
-      animateProgress.mode = (EzLerp.Mode)targetMode;
-      _plateObject.transform.localPosition = animateProgress.Add(_positionDefault, _positionDelta);
-      if (_plateMaterial)
+      while (Application.isPlaying)
       {
-        _plateMaterial.color = animateProgress.Mix(_relaxColor, _pressColor);
+        animateProgress.mode = (EzLerp.Mode)targetMode;
+        _plateObject.transform.localPosition = animateProgress.Add(_positionDefault, _positionDelta);
+        if (_plateMaterial)
+        {
+          _plateMaterial.color = animateProgress.Mix(_relaxColor, _pressColor);
+        }
+        await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
       }
-      await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
+      _plateObject.transform.localPosition = _positionDefault;
     }
-    _plateObject.transform.localPosition = _positionDefault;
   }
 }
