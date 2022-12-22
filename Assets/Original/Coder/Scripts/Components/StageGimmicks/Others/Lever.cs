81216440b1d4e947086a54b3c860dbb3fde129a9
kabutogani <a20dc119@dhw.ac.jp>
2022-12-23 00:54:00 +0900

    デバッグ用レバーのスクリプトを追加、レバー用デバッグルームを整理。

--- Assets/Original/Coder/Scripts/Components/StageGimmicks/Others/Lever.cs
+++ Assets/Original/Coder/Scripts/Components/StageGimmicks/Others/Lever.cs
@@ -4,32 +4,54 @@ using UnityEngine;
 using Utilities;
 using Assembly.GameSystem.Message;
 using UniRx;
-using System;
+using Assembly.Components.Actors.Player;
 
 namespace Assembly.Components.StageGimmicks{
 [RequireComponent(typeof(SafetyTrigger))]
     public class Lever : MonoBehaviour
     {   
         SafetyTrigger safetyTrigger;
-        // enum Switch { Off = -1, On = 1 }
-        // Switch targetSwitch = Switch.Off;
-        bool switchs;
+        public BoolReactiveProperty _targetSwitch;
         [SerializeField] MessageDispatcher _OnSwitch = new MessageDispatcher();
-        EzLerp messageSwitch = new EzLerp(1);
-        [SerializeField]MixFactor mixFactor;
+
 
         // Start is called before the first frame update
         void Start()
         {
             safetyTrigger = GetComponent<SafetyTrigger>();
-            //_OnSwitch.message.intensity = mixFactor.;
+            
+            _targetSwitch.Where(_ => _targetSwitch.Value == true).Subscribe(_ => _OnSwitch.message.intensity.SetFactor1());
+            _targetSwitch.Where(_ => _targetSwitch.Value == false).Subscribe(_ => _OnSwitch.message.intensity.SetFactor0());
+            Global.Control.InteractInput.Buffer(2).Subscribe(_ => SwitchOn_Off());
+            
         }
 
         // Update is called once per frame
         void Update()
         {
             _OnSwitch.Dispatch();
-            //Debug.Log(safetyTrigger.triggers[0]);
+        }
+
+        private void SwitchOn_Off(){
+            if(JudgeTriggerEnter()){
+                if(_targetSwitch.Value){
+                    _targetSwitch.Value = false;
+                }else{
+                    _targetSwitch.Value = true;
+                }
+            }
+        }
+
+        private bool JudgeTriggerEnter(){
+            bool onPlayer = false;
+            if(safetyTrigger.triggers != null){
+                for(int i =0; i < safetyTrigger.triggers.Count; i++){
+                    if(safetyTrigger.triggers[i].gameObject.GetComponent<PlayerAct>() != null){
+                        onPlayer = true;
+                    };
+                }
+            }
+            return onPlayer;
         }
     }
 }
