21ec5919a0a73b0422e091288117b0c0e7a5b064
panno <panno.800k@gmail.com>
2023-01-17 15:33:42 +0900

    カメラシステムを更新

--- Assets/Original/Coder/Scripts/Components/Cameraman/CamctlOverride.cs
+++ Assets/Original/Coder/Scripts/Components/Cameraman/CamctlOverride.cs
@@ -1,57 +1,38 @@
 using UnityEngine;
 using UniRx;
-using UniRx.Triggers;
 using Cinemachine;
 using Assembly.GameSystem;
-using Assembly.Components.Actors.Player;
 
 namespace Assembly.Components
 {
+  [RequireComponent(typeof(SafetyTrigger))]
   public class CamctlOverride : MonoBehaviour
   {
-    PlayerAct player;
-    [Zenject.Inject]
-    public void DepsInject(PlayerAct player)
-    {
-      this.player = player;
-    }
-
     public enum OverrideMode { Temporary, Forever }
     [SerializeField] CinemachineVirtualCamera _cmCamera;
     [SerializeField] OverrideMode _overrideMode;
-    [SerializeField] int priority = 20;
-    [SerializeField] bool followPlayer;
+    [SerializeField] bool followEncounter;
+
+    SafetyTrigger trigger;
 
     void Awake()
     {
-      this.OnTriggerEnterAsObservable()
+      trigger = GetComponent<SafetyTrigger>();
+
+      trigger.OnEnter
           .Where(other => other.CompareTag(Tag.CtlvolCamera.GetName()))
           .Subscribe(other =>
           {
-            if (followPlayer)
-            {
-              _cmCamera.m_Follow = player.transform;
-            }
-            _cmCamera.Priority = priority;
-          });
-      this.OnTriggerExitAsObservable()
-          .Where(other => _overrideMode == OverrideMode.Temporary)
-          .Where(other => other.CompareTag(Tag.CtlvolCamera.GetName()))
-          .Subscribe(other =>
-          {
-            _cmCamera.Priority = 0;
-            if (followPlayer && _cmCamera.m_Follow == player.transform)
-            {
-              _cmCamera.m_Follow = null;
-            }
+            if (followEncounter)
+            { _cmCamera.m_Follow = other.transform; }
+
+            _cmCamera.enabled = true;
           });
 
-      player.OnAssembleObservable
-          .Subscribe(_ =>
-          {
-            _cmCamera.Priority = 0;
-          }).AddTo(this);
-
+      trigger.OnExit
+        .Where(other => _overrideMode == OverrideMode.Temporary)
+        .Where(other => other.CompareTag(Tag.CtlvolCamera.GetName()))
+        .Subscribe(other => _cmCamera.enabled = false);
     }
   }
 }
