9d1f154ff4dc121e1fcaeef63059ef652ea5b63b
panno <panno.800k@gmail.com>
2023-01-22 21:58:14 +0900

    WaterVolume*の処理を最適化

---
+++ Assets/Plugins/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WaterVolumeFloater.cs
@@ -0,0 +1,44 @@
+﻿#region Using statements
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion
+
+namespace Bitgem.VFX.StylisedWater
+{
+    public class WaterVolumeFloater : MonoBehaviour
+    {
+        #region Public fields
+
+        public WaterVolumeHelper WaterVolumeHelper = null;
+
+        #endregion
+
+        #region MonoBehaviour events
+
+        float __height;
+        WaterVolumeHelper __instance;
+
+        void Update()
+        {
+            if (!__instance)
+            {
+                __instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
+                if (!__instance)
+                {
+                    return;
+                }
+            }
+
+            if (__instance.GetHeight(transform.position, out __height))
+            {
+                transform.position = new Vector3(transform.position.x, __height, transform.position.z);
+            }
+
+        }
+
+        #endregion
+    }
+}
--- Assets/Plugins/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WaterVolumeHelper.cs
+++ Assets/Plugins/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WaterVolumeHelper.cs
@@ -10,6 +10,14 @@ namespace Bitgem.VFX.StylisedWater
 {
     public class WaterVolumeHelper : MonoBehaviour
     {
+        #region Shader IDs
+
+        static int idWaveFrequency = Shader.PropertyToID("_WaveFrequency");
+        static int idWaveScale = Shader.PropertyToID("_WaveScale");
+        static int idWaveSpeed = Shader.PropertyToID("_WaveSpeed");
+
+        #endregion
+
         #region Private static fields
 
         private static WaterVolumeHelper instance = null;
@@ -30,33 +38,36 @@ namespace Bitgem.VFX.StylisedWater
 
         #region Public methods
 
-        public float? GetHeight(Vector3 _position)
+        public bool GetHeight(Vector3 _position, out float height)
         {
+            height = 0;
+
             // ensure a water volume
             if (!WaterVolume)
             {
-                return 0f;
+                return false;
             }
 
             // ensure a material
             var renderer = WaterVolume.gameObject.GetComponent<MeshRenderer>();
             if (!renderer || !renderer.sharedMaterial)
             {
-                return 0f;
+                return false;
             }
 
             // replicate the shader logic, using parameters pulled from the specific material, to return the height at the specified position
             var waterHeight = WaterVolume.GetHeight(_position);
             if (!waterHeight.HasValue)
             {
-                return null;
+                return false;
             }
-            var _WaveFrequency = renderer.sharedMaterial.GetFloat("_WaveFrequency");
-            var _WaveScale = renderer.sharedMaterial.GetFloat("_WaveScale");
-            var _WaveSpeed = renderer.sharedMaterial.GetFloat("_WaveSpeed");
+            var _WaveFrequency = renderer.sharedMaterial.GetFloat(idWaveFrequency);
+            var _WaveScale = renderer.sharedMaterial.GetFloat(idWaveScale);
+            var _WaveSpeed = renderer.sharedMaterial.GetFloat(idWaveSpeed);
             var time = Time.time * _WaveSpeed;
             var shaderOffset = (Mathf.Sin(_position.x * _WaveFrequency + time) + Mathf.Cos(_position.z * _WaveFrequency + time)) * _WaveScale;
-            return waterHeight.Value + shaderOffset;
+            height = waterHeight.Value + shaderOffset;
+            return true;
         }
 
         #endregion
